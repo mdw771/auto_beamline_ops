Adaptive point density for acqf weight and early stopping; fix early stopping bug; build acqf weighting finds highest gradient peak instead of the first one

autobl/steering/guide.py
@@ -35,8 +35,9 @@ class StoppingCriterion:
 
     def check_max_uncertainty(self):
         t = self.configs.params['threshold']
-        x = torch.linspace(0, 1, 100).view(-1, 1)
+        x = torch.linspace(0, 1, self.guide.data_x.shape[0] * 5).view(-1, 1)
         mu, sigma = self.guide.get_posterior_mean_and_std(x, transform=False, untransform=True)
+        sigma = sigma.squeeze()
         if self.guide.acqf_weight_func is not None:
             w = torch.clip(self.guide.acqf_weight_func(x).squeeze(), 0, 1)
         else:
@@ -353,7 +354,7 @@ class XANESExperimentGuide(GPExperimentGuide):
             logging.warning('Acquisition function does not have attribute "set_weight_func".')
             self.acqf_weight_func = None
             return
-        x = torch.linspace(0, 1, 100).reshape(-1, 1)
+        x = torch.linspace(0, 1, self.data_x.shape[0] * 10).reshape(-1, 1)
         mu, _ = self.get_posterior_mean_and_std(x, transform=False)
         mu = mu.squeeze()
         # convert 3 eV to pixel
@@ -364,10 +365,11 @@ class XANESExperimentGuide(GPExperimentGuide):
 
         # convert 3 eV to pixel
         min_peak_width = float(3.0 / (self.input_transform.bounds[1][0] - self.input_transform.bounds[0][0]) * len(x))
-        peak_inds, peak_properties = scipy.signal.find_peaks(mu_grad, height=0.05, width=min_peak_width)
+        peak_locs, peak_properties = scipy.signal.find_peaks(mu_grad, height=0.05, width=min_peak_width)
+        max_peak_ind = np.argmax(peak_properties['peak_heights'])
 
-        peak_loc_normalized = float(peak_inds[0]) / len(x)
-        peak_width_normalized = peak_properties['widths'][0] / len(x)
+        peak_loc_normalized = float(peak_locs[max_peak_ind]) / len(x)
+        peak_width_normalized = peak_properties['widths'][max_peak_ind] / len(x)
 
         def weight_func(x):
             m = sigmoid(x, r=20. / peak_width_normalized, d=peak_loc_normalized - 1.6 * peak_width_normalized)

workspace/spectroscopy/XANES/XANES_sampling_LTO.py
@@ -72,7 +72,7 @@ configs = XANESExperimentGuideConfig(
     # optimizer_params={'torch_optimizer': torch.optim.Adam, 'torch_optimizer_options': {'maxiter': 20}},
     stopping_criterion_configs=StoppingCriterionConfig(
         method='max_uncertainty',
-        params={'threshold': 0.1}
+        params={'threshold': 0.05}
     )
 )
 

workspace/spectroscopy/XANES/XANES_sampling_YBCO_full.py
@@ -36,6 +36,7 @@ data = data_all_spectra['YBCO_epararb.0001'].to_numpy()
 ref_spectra_0 = torch.tensor(data_all_spectra['YBCO_epara.0001'].to_numpy())
 ref_spectra_1 = torch.tensor(data_all_spectra['YBCO_eparc.0001'].to_numpy())
 energies = data_all_spectra['energy'].to_numpy()
+print('Energy range = {} eV'.format(energies[-1] - energies[0]))
 energies = torch.tensor(energies)
 y_fit = linear_fit([to_numpy(ref_spectra_0), to_numpy(ref_spectra_1)], data)
 fig, ax = plt.subplots(1, 1, figsize=(5, 3))
@@ -55,7 +56,7 @@ configs = XANESExperimentGuideConfig(
     model_class=botorch.models.SingleTaskGP,
     model_params={'covar_module': gpytorch.kernels.MaternKernel(2.5)},
     noise_variance=1e-6,
-    override_kernel_lengthscale=7,
+    override_kernel_lengthscale=10,
     lower_bounds=torch.tensor([energies[0]]),
     upper_bounds=torch.tensor([energies[-1]]),
     acquisition_function_class=ComprehensiveAugmentedAcquisitionFunction,
@@ -79,7 +80,7 @@ configs = XANESExperimentGuideConfig(
     optimizer_class=DiscreteOptimizer,
     optimizer_params={'optim_func': botorch.optim.optimize.optimize_acqf_discrete,
                       'optim_func_params': {
-                          'choices': torch.linspace(0, 1, 1000)[:, None]
+                          'choices': torch.linspace(0, 1, 5000)[:, None]
                       }
                      },
 
@@ -95,11 +96,11 @@ configs = XANESExperimentGuideConfig(
 )
 
 analyzer_configs = ExperimentAnalyzerConfig(
-    name='YBCO3data',
+    name='YBCO3dataFull',
     output_dir='outputs',
     n_plot_interval=5
 )
 
 experiment = SimulatedScanningExperiment(configs, run_analysis=True, analyzer_configs=analyzer_configs)
 experiment.build(energies, data)
-experiment.run(n_initial_measurements=200, n_target_measurements=270)
+experiment.run(n_initial_measurements=140, n_target_measurements=270)