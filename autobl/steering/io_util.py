e0d7620926e5d9322afe42f0452e1f2508cb98d3
Ming Du <ming.du101@gmail.com>
2024-06-27 16:02:38 -0500

    Spectroscopy IO classes

--- autobl/steering/io_util.py
+++ autobl/steering/io_util.py
@@ -1,7 +1,72 @@
 import io
+import glob
+import os
 
 import pandas as pd
 import numpy as np
+
+
+class SpectroscopyDataset:
+
+    def __init__(self, path, *args, **kwargs):
+        self.path = path
+        self.data = None
+        self.energies_ev = None
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+
+class ColumnMajorCSVSpectroscopyDataset(SpectroscopyDataset):
+    def __init__(self, *args, column_name="YBCO_epararb.0001", **kwargs):
+        super().__init__(*args, **kwargs)
+        table = pd.read_csv(self.path, header=0)
+        self.data = table[column_name].to_numpy()
+        self.data = self.data.reshape(1, -1)
+        self.energies_ev = table['energy'].to_numpy()
+
+
+class YBCORawDataset(SpectroscopyDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        table = pd.read_table(self.path, comment='#', header=None, sep='\s+')
+        self.energies_ev = table[0].to_numpy()
+        self.data = (table[4] / table[5]).to_numpy()
+        self.data = self.data.reshape(1, -1)
+
+
+class RowMajorCSVSpectroscopyDataset(SpectroscopyDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        table = pd.read_csv(self.path, header=None)
+        self.data = table.iloc[1:].to_numpy()
+        self.energies_ev = table.iloc[0].to_numpy()
+
+
+class NORSpectroscopyDataset(SpectroscopyDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        flist = glob.glob(os.path.join(self.path, '*.nor'))
+        flist.sort()
+        self.data = []
+        for f in flist:
+            d, self.energies_ev = self.load_data_from_nor(f)
+            self.data.append(d)
+        self.data = np.array(self.data)
+
+    @staticmethod
+    def load_data_from_nor(path):
+        table = read_nor(path)
+        data = table['norm'].to_numpy()
+        energies = table['e'].to_numpy()
+        _, unique_inds = np.unique(energies, return_index=True)
+        unique_inds = np.sort(unique_inds)
+        data = data[unique_inds]
+        energies = energies[unique_inds]
+        return data, energies
 
 
 def read_nor(fname):
