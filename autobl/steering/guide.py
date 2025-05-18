FIX: scale noise variance for updates

autobl/steering/guide.py
@@ -446,7 +446,9 @@ class GPExperimentGuide(ExperimentGuide):
         self.record_data(x_data, y_data)
         additional_params = {}
         if self.config.noise_variance is not None:
-            additional_params['noise'] = torch.full_like(y_data, self.config.noise_variance)
+            noise_std = self.config.noise_variance ** 0.5
+            noise_std = self.scale_by_standardizer_scale(noise_std)
+            additional_params['noise'] = torch.full_like(y_data, noise_std ** 2)
         new_model = self.model.condition_on_observations(x_data, y_data, **additional_params)
         # In-place update all attribute in self.model so that all references get updated.
         self.model.__dict__ = new_model.__dict__
@@ -471,7 +473,8 @@ class GPExperimentGuide(ExperimentGuide):
         gpytorch.models.GP
             The GP model object.
         """
-        assert not ('train_Yvar' in self.config.model_params.keys() and self.config.noise_variance is not None)
+        assert not ('train_Yvar' in self.config.model_params.keys() and self.config.noise_variance is not None), \
+            "train_Yvar should not be in model_params when noise_variance is provided."
         additional_params = {}
         extra_params = {} if extra_params is None else extra_params
         if self.config.noise_variance is not None:

autobl/steering/util.py
@@ -2,11 +2,11 @@ import torch
 import numpy as np
 
 
-def estimate_noise_std(x: np.ndarray, y: np.ndarray) -> float:
-    """Estimate the noise standard deviation of the given 1D data.
+def estimate_noise_variance(x: np.ndarray, y: np.ndarray) -> float:
+    """Estimate the noise variance of the given 1D data.
     The data is assumed to only have a linear background, which will
-    be fit and subtracted. The noise standard deviation is then taken
-    as the standard deviation of the residuals.
+    be fit and subtracted. The noise variance is then taken
+    as the variance of the residuals.
     
     Parameters
     ----------
@@ -17,12 +17,12 @@ def estimate_noise_std(x: np.ndarray, y: np.ndarray) -> float:
 
     Returns
     -------
-    noise_std : float
-        The estimated noise standard deviation.
+    noise_variance : float
+        The estimated noise variance.
     """
     # Fit a linear background
     fit = np.polyfit(x, y, 1)
     background = np.polyval(fit, x)
     residuals = y - background
-    noise_std = np.std(residuals)
-    return noise_std
+    noise_variance = np.var(residuals)
+    return noise_variance